31171a9c99fd5013ae09a3e416dd18017c196586
bch-af_fun0013 <bch-af_fun0013@flender-group.com>
2024-03-22 11:21:59 +0100

    final tour of heroes

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,15 +1,3 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-hero-search',
-  standalone: true,
-  imports: [],
-  templateUrl: './hero-search.component.html',
-  styleUrl: './hero-search.component.css'
-})
-export class HeroSearchComponent {
-
-}
 import { Component, OnInit } from '@angular/core';
 
 import { Observable, Subject } from 'rxjs';
--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class MessageService {
+  messages: string[] = [];
 
-  constructor() { }
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
 }
-import { MessageService } from './message.service';
-
